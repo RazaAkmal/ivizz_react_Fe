f58904b3ce1239d1c4fcebb17c2f722892d3761c
Raza Akmal <razaakmal11@gmail.com>
2022-01-19 22:36:43 +0500

    reverted the change

--- src/common/components/BarChart.js
+++ src/common/components/BarChart.js
@@ -4,7 +4,7 @@ import { Bar } from 'react-chartjs-2'
 const BarChart = (props) => {
 
   return (
-    <Bar data={props.data || []} getElementAtEvent={props.handleChartClick} options={props.options} />
+    <Bar data={props.data} getElementAtEvent={props.handleChartClick} options={props.options} />
   )
 }
 
